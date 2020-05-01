5d4b98ab00f98c84f4f97509bb3d61bd527640dc
Nacchofer31 <ignaciofer31@gmail.com>
2020-05-01 13:51:21 +0200

    Added redirect full to panel

--- exerciseList/src/app/app-routing.module.ts
+++ exerciseList/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
+import { PanelComponent } from './panel/panel.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ExercisesComponent } from './exercises/exercises.component';
 
 
 const routes: Routes = [
-  {path:'exercises',component:ExercisesComponent}
+  {path:'exercises',component:ExercisesComponent},
+  {path:'panel',component:PanelComponent},
+  {path:'',redirectTo:'/panel',pathMatch:'full'}
 ];
 
 @NgModule({
