8dd9dbe33fefeac3a98b8e5ac7c15659864e612e
Nacchofer31 <ignaciofer31@gmail.com>
2020-05-04 13:21:42 +0200

    Added detail route

--- exerciseList/src/app/app-routing.module.ts
+++ exerciseList/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
+import { ExerciseDetailComponent } from './exercise-detail/exercise-detail.component';
 import { PanelComponent } from './panel/panel.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ExercisesComponent } from './exercises/exercises.component';
 
-
 const routes: Routes = [
   {path:'exercises',component:ExercisesComponent},
+  {path:'detail/:id',component:ExerciseDetailComponent},
   {path:'panel',component:PanelComponent},
   {path:'',redirectTo:'/panel',pathMatch:'full'}
 ];
