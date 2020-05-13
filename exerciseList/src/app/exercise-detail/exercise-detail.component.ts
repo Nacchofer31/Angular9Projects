39a06a9ac8b9c3d4a52ebcc1fe698f2451639465
Nacchofer31 <ignaciofer31@gmail.com>
2020-05-13 13:50:54 +0200

    Added back function with Location

--- exerciseList/src/app/exercise-detail/exercise-detail.component.ts
+++ exerciseList/src/app/exercise-detail/exercise-detail.component.ts
@@ -2,6 +2,7 @@ import { ExerciseService } from './../exercise.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { Exercise } from '../exercise';
 import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-exercise-detail',
@@ -12,7 +13,9 @@ export class ExerciseDetailComponent implements OnInit {
 
   exercise:Exercise;
 
-  constructor(private route:ActivatedRoute, private exerciseService:ExerciseService) { }
+  constructor(private route:ActivatedRoute,
+              private exerciseService:ExerciseService,
+              private location:Location) { }
 
   ngOnInit(): void {
     this.getExercise()
@@ -21,6 +24,10 @@ export class ExerciseDetailComponent implements OnInit {
   getExercise():void{
     const id =+ this.route.snapshot.paramMap.get('id')
     this.exerciseService.getExerciseById(id).subscribe(exercise=>this.exercise=exercise)
+  }
+
+  goBack():void{
+    this.location.back()
   }
 
 }
